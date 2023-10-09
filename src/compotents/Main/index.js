37f2e628cc455cacc4a0ffc11fc447cc39133a31
roman.darmosyuk <roman.darmosyuk@yandex.ru>
2023-10-10 01:31:42 +0300

    started main

--- src/compotents/Main/index.js
+++ src/compotents/Main/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { Wrapper } from "../Wrapper";
 import Input from "../Input"
 import { Button } from "../Button";
+import Advantages from "../Advantages";
 
 const Main = () => {
    return (
@@ -26,6 +27,19 @@ const Main = () => {
             </Button>
          </div>
          </Wrapper>
+         <div className="main__hired hired">
+            <Wrapper>
+            <div className="hired__container">
+               <h2 className="hired__title">Your new path to hired.</h2>
+               <Button className="button-lime">
+                  <p className="hired__button-text">Get started free</p>
+               </Button>
+            </div>
+            <div className="hired__advantages">
+               <Advantages/>
+            </div>
+            </Wrapper>    
+         </div>
       </div>
    );
 };
